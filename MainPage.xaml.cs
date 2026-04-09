6890e0959d9c44e5c087ac085d491e4e97ee65c3
MarynaSteniuk <stenyukmarina1@gmail.com>
2026-04-09 21:44:00 +0300

    Виконана 1 вправа 4 лабораторної

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,24 +9,20 @@ public partial class MainPage : ContentPage
 
     private async void btnRefresh_Clicked(object sender, EventArgs e)
     {
+     
         btnRefresh.IsEnabled = false;
         actIsBusy.IsRunning = true;
 
-        Models.WeatherData weatherData = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
-
-        lblWind.Text = weatherData.Wind.ToString();
-        lblHumidity.Text = weatherData.Humidity.ToString();
-        lblPrecipitation.Text = weatherData.Precipitation.ToString();
-        lblTemperature.Text = weatherData.Temperature.ToString("#");
-
-        imgCondition.Source = weatherData.Condition switch
+        try
         {
-            Models.WeatherType.Sunny => ImageSource.FromFile("sunny.png"),
-            Models.WeatherType.Cloudy => ImageSource.FromFile("cloud.png"),
-            _ => ImageSource.FromFile("question.png")
-        };
+            var weatherData = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
 
-        btnRefresh.IsEnabled = true;
-        actIsBusy.IsRunning = false;
+            BindingContext = weatherData;
+        }
+        finally
+        {
+            btnRefresh.IsEnabled = true;
+            actIsBusy.IsRunning = false;
+        }
     }
 }
